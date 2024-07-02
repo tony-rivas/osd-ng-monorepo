70af980c2d5563fc77dea4c17ebab11672c99cae
Tony Rivas <tony@tonyrivas.org>
2024-07-02 15:25:34 -0700

    first commit of osd

--- apps/home/src/app/app.component.spec.ts
+++ apps/home/src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
+/* RouterTestingModule deprecated see fix below
+https://github.com/angular/angular-cli/pull/27851 */
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
   });
 
@@ -14,7 +15,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome home');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome Home');
   });
 
   it(`should have as title 'home'`, () => {
